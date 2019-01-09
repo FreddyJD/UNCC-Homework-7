ecfb79dfbdaf435d20a7edfa9ac8ecff0697bc0f
Freddy <freddy.jd2@gmail.com>
2019-01-09 00:58:27 -0500

    Initial files

---
+++ app.js
@@ -0,0 +1,19 @@
+// Create DOM 
+//  Pass variables Train Name --- Destination --- Frequency (min) --- Next arrival --- Minutes Away 
+// Create a function that listen to the DB cloud 
+// Push data to the DB and 
+// Listener DB (should push the passHTML)
+
+function passHTML(data) {
+    var htmlData = `
+    <tr>
+        <th scope="col"></th>
+        <th scope="col"></th>
+        <th scope="col"></th>
+        <th scope="col"></th>
+        <th scope="col"></th>
+        <th scope="col"></th>
+    </tr>`
+}
+
+
