de527ee09858a0a5ca09eb03515cf2933218fdea
Freddy <freddy.jd2@gmail.com>
2019-01-10 00:42:18 -0500

    Files updated 🔥 Working

--- app.js
+++ app.js
@@ -1,51 +1,72 @@
-// Create DOM 
-//  Pass variables Train Name --- Destination --- Frequency (min) --- Next arrival --- Minutes Away 
-// Create a function that listen to the DB cloud 
-// Push data to the DB and 
-// Listener DB (should push the passHTML)
-
+var config = {
+    apiKey: "AIzaSyA_zPKqw-r0wD5EPjrBT84sXA7SrGcAMuo",
+    authDomain: "daboclock-demo.firebaseapp.com",
+    databaseURL: "https://daboclock-demo.firebaseio.com",
+    projectId: "daboclock-demo",
+    storageBucket: "daboclock-demo.appspot.com",
+    messagingSenderId: "533607149029"
+  };
 
 firebase.initializeApp(config);
 var database = firebase.database();
+var refDir = database.ref('/entries');
 
-ref.on("value", function(snapshot){ 
-    htmlData()
+
+refDir.on("child_added", function(snap){
+        $("tbody").append(passHTML(snap.val()));
 });
 
-$("#add-user").on("click", function(event) {
+$("#add").on("click", function(event) {
     event.preventDefault();
+    console.log("Works!")
 
-    InputName = $("#Train-name-input").val().trim();
-    InputDestination = $("#Destination-input").val().trim();
-    InputFreq = $("#Frequency-input").val().trim();
-    InputNext = $("#Next-input").val().trim();
-    // We will need this later ❤️ (Get data and update it!)
+    var name = $("#Train-name-input").val();
+    var destination = $("#Destination-input").val();
+    var freq = $("#Frequency-input").val();
+    var first = $("#Next-input").val();
+
+    refDir.push({
+      name,
+      destination,
+      freq,
+      first
+
+    });
+  });
+
+function passHTML(data) {
+    var firstConv = moment(`${data.first}`, "HH:mm").subtract(1, "years");
+    var diference = moment().diff(moment(firstConv), "minutes");
+    var getRemainder = diference % `${data.freq}`;
+    var getLeft = `${data.freq}` - getRemainder;
+    console.log(getLeft);
+
+    var momentGet = moment();
+
+    var whatHour = moment(momentGet, "hh:mm:ss A")
+    .add(0, 'seconds')
+    .add(getLeft, 'minutes')
+    .format('LTS');
+
+
+    var htmlData = $('tbody').append(`
+    <tr>
+        <th scope="col">${data.name}</th>
+        <th scope="col">${data.destination}</th>
+        <th scope="col">${data.freq}</th>
+        <th scope="col">${whatHour}</th>
+        <th scope="col">${getLeft}</th>
+
+
+    </tr>`);
+    return htmlData;
+};
+
+
+function trainLogic() { 
     let getTransform = moment(InputDestination, "HH:mm").subtract(1, "years");
     let getTime = moment();
     let getDif = getTime.diff(moment(getTransform), "minutes");
     let getRemainder = getDif % InputFreq;
     let away = InputFreq - getRemainder;
-
-    database.ref().set({
-      name,
-      destination,
-      freq,
-      next,
-      away
-    });
-
-  });
-
-function passHTML(data) {
-    var htmlData = `
-    <tr>
-        <th scope="col">${data.name}</th>
-        <th scope="col">${data.destination}</th>
-        <th scope="col">${data.freq}</th>
-        <th scope="col">${data.next}</th>
-        <th scope="col">${data.away }</th>
-    </tr>`
-    return htmlData;
-}
-
-
+};
